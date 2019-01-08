7cb2f7deb693d9e86b79acbc316ae0c89d32ba81
Taylor <Pasquariello>
2019-01-08 11:34:54 -0700

    Updates

---
+++ precache-manifest.578c319bf6281a18121e40c0c964982f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "17d24c8af74755b5cc75",
+    "url": "/react-todo/static/js/main.17d24c8a.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/react-todo/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "17d24c8af74755b5cc75",
+    "url": "/react-todo/static/css/main.f30d7727.chunk.css"
+  },
+  {
+    "revision": "2f62dff495cc3b5ef4e460e6ea0e12a2",
+    "url": "/react-todo/index.html"
+  }
+];
